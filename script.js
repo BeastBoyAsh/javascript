8ad58ba4435ab601c952894f5e164ffb23d7d23e
Ayush Singh <86451528+BeastBoyAsh@users.noreply.github.com>
2023-04-29 22:09:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+
+  let js='amazing';
+  if (js='amazing') alert("JAVASCRIPT IS FUN");
+  
+  console.log(10+8+2-2);
+
+  console.log("Jonas");
+  console.log(23);
+
+  let firstName = "Bob"; 
+
+  let first
+  let firstNamePerson //CARMEL-CASE and _,$ are allowed
+
+  console.log(firstName);
+  console.log(firstName);
+  console.log(firstName);
+
+  let myFirstJob='Programmer';
+  let myCurrentJob='Teacher';
+
+  let job1='programmer';
+  let job2='teacher';
+
+  console.log(myFirstJob);
+
+  let PI=3.145;
+  true;
+
+
+  let javascriptIsFun=true;
+  console.log(javascriptIsFun);
+
+  console.log(typeof true); 
+  console.log(typeof javascriptIsFun);
+  console.log(typeof 23);
+  console.log(typeof "Jonas");
+  console.log(typeof 'Jonas');
+  console.log(typeof Jonas);
+
+  javascriptIsFun='YES!';
+  console.log(typeof javascriptIsFun);
+
+  let year;
+  console.log(year);
+  console.log(typeof year);
+
+  year=2001;
+  console.log(year);
+  console.log(typeof year);
+
+  console.log(typeof null);
+
+
+let age=30;
+age=31;
+
+const birthYear=1991; //IMMUTABLE
+// birthYear=1990;
+
+//const job;
+
+var job='programmer';
+job='teacher';// don't use VAR
+
+lastName='Singh';
+console.log(lastName);//don't write it like this
+
+
+const now=2023;
+const ageAyush=now-2001;
+const ageAryan=now-2003;
+console.log(ageAyush,ageAryan);
+
+console.log(ageAryan*2,ageAyush/10);
+
+const firstName='Ayush';
+const lastName='Singh';
+
+console.log(firstName+' '+lastName);
+
+let x=10+5;
+console.log(x);
+
+
+console.log(ageAryan>ageAyush);//>,<,>=,<=
+console.log(ageAryan>=20);
